89b6bfa15cde8633036ea4e28dfa66d3863aaba6
wwright <wwright@methode.com>
2016-05-02 09:54:30 -0500

    page 80. using LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,12 +1,13 @@
 #/usr/bin/env python3
 
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import unittest
 import time
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
@@ -23,6 +24,8 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes 
         # to check out its homepage
+        self.browser.get(self.live_server_url)
+        
         self.browser.get('http://localhost:8000')
         self.assertIn('To-Do',self.browser.title)
         header_text = self.browser.find_element_by_tag_name('h1').text
